53389d6e5b3a21580b0b6826fd1952ebb6e452ae
Michael Boone <fuag155555@gmail.com>
2017-08-14 16:55:20 -0700

    initial commit

---
+++ CMakeFiles/Meld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "compile_shaders.sh"
+  "run.sh"
+  "CMakeFiles/Meld.dir/main.cpp.o"
+  "Meld.pdb"
+  "Meld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Meld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
